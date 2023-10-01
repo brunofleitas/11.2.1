82dc06099168f4e4cee92ea8b609c9b8d61b7355
brunofleitas <brunofleitas2011@gmail.com>
2023-10-01 11:13:09 +0200

    Se soluciono error fetch

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-document.getElementById("registro").addEventListener("submit", function(enent){
+document.getElementById("registro").addEventListener("submit", function(event){
+    event.preventDefault();
     const nombre = document.getElementById("nombre").value;
     const apellido = document.getElementById("apellido").value;
     const fechaNacimiento = document.getElementById("fechaNacimiento").value;
 
-    let datos = {
+    const data = {
         nombre: nombre,
         apellido: apellido,
         fechaNacimiento: fechaNacimiento
@@ -11,11 +12,18 @@ document.getElementById("registro").addEventListener("submit", function(enent){
 
     fetch("https://jsonplaceholder.typicode.com/users", {
         method: "POST",
-        body: JSON.stringify(datos),
+        body: JSON.stringify(data),
         headers:{
-            "Content-Type": "application/json"
-        }
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
     })
 
+    .then(response => response.json())
+    .then(data => {
+        console.log('Respuesta del servidor:', data);
+    })
+
+    .catch(error => console.error('Error:', error));
     
 });
